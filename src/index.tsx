2e4bb87f4255343a97ef81626a5661763e1fb71c
Dawson Booth <dawson@dawsonbooth.com>
2022-04-29 15:44:57 -0500

    Set up server state provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './home'
 import './index.css'
 import Projects from './projects'
 import reportWebVitals from './reportWebVitals'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      staleTime: Infinity,
+    },
+  },
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="projects" element={<Projects />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools position="bottom-right" />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
 
