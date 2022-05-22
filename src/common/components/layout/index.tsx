8effce7d2239bcc942f0e7bb649caf0e514016f3
Dawson Booth <dawson@dawsonbooth.com>
2022-05-22 17:05:49 -0500

    Update background color

--- src/common/components/layout/index.tsx
+++ src/common/components/layout/index.tsx
@@ -10,7 +10,7 @@ const Layout: React.FC<Props> = ({ children }) => {
   const navigate = useNavigate()
 
   return (
-    <div className="flex flex-col items-stretch min-h-screen w-screen bg-slate-400">
+    <div className="flex flex-col items-stretch min-h-screen w-screen bg-slate-100">
       <header className="flex justify-center shadow-md bg-white">
         <div className="flex gap-2 p-2 max-w-screen-lg">
           {Object.values(routes).map(route => (
