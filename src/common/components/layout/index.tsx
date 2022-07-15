fe8f901dd169917e3c80efb1e28f85c1760eb758
Dawson Booth <dawson@dawsonbooth.com>
2022-07-14 19:16:30 -0500

    Change layout nav buttons to links

--- src/common/components/layout/index.tsx
+++ src/common/components/layout/index.tsx
@@ -1,32 +1,24 @@
 import React from 'react'
-import { useNavigate } from 'react-router'
+import { Link } from 'react-router-dom'
 import routes from '../../providers/router/routes'
 
 interface Props {
   children: React.ReactNode | React.ReactNode[]
 }
 
-const Layout: React.FC<Props> = ({ children }) => {
-  const navigate = useNavigate()
-
-  return (
-    <div className="flex flex-col items-stretch min-h-screen w-screen bg-slate-100">
-      <header className="flex justify-center shadow-md bg-white">
-        <div className="flex gap-2 p-2 max-w-screen-lg">
-          {Object.values(routes).map(route => (
-            <button
-              key={route.name}
-              onClick={() => navigate(route.path)}
-              className="p-2 rounded-md active:bg-slate-100"
-            >
-              {route.name}
-            </button>
-          ))}
-        </div>
-      </header>
-      <main className="flex-1">{children}</main>
-    </div>
-  )
-}
+const Layout: React.FC<Props> = ({ children }) => (
+  <div className="flex flex-col items-stretch min-h-screen w-screen bg-slate-100">
+    <header className="flex justify-center shadow-md bg-white">
+      <div className="flex gap-2 p-2 max-w-screen-lg">
+        {Object.values(routes).map(route => (
+          <Link to={route.path} key={route.name} className="p-2 rounded-md active:bg-slate-100">
+            {route.name}
+          </Link>
+        ))}
+      </div>
+    </header>
+    <main className="flex-1">{children}</main>
+  </div>
+)
 
 export default Layout
