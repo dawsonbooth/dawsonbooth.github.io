45e6f9af852bbe65efaffcd20229760d954426d3
Dawson Booth <dawson@dawsonbooth.com>
2022-09-20 21:21:42 -0500

    Use classnames library for dynamic styles

--- src/projects/components/tag/index.tsx
+++ src/projects/components/tag/index.tsx
@@ -1,23 +1,22 @@
-import { mix } from 'polished'
+import classNames from 'classnames'
 import * as React from 'react'
 
 interface Props {
-  hue: number
   active: boolean
   onClick: () => void
   children: string
 }
 
-const Tag: React.FC<Props> = ({ hue, active, onClick, children }) => {
+const Tag: React.FC<Props> = ({ active, onClick, children }) => {
   return (
     <button
       onClick={onClick}
-      className="select-none whitespace-nowrap rounded-full px-2 py-1 text-sm"
-      style={{
-        // TODO: Investigate style prop usage for dynamic values
-        backgroundColor: mix(0.5, `hsl(${hue}, 60%, 75%)`, 'white'),
-        opacity: active ? 1 : 0.5,
-      }}
+      className={classNames(
+        'select-none whitespace-nowrap rounded-full bg-slate-200 px-2 py-1 text-sm',
+        {
+          'opacity-50': !active,
+        },
+      )}
     >
       {children}
     </button>
